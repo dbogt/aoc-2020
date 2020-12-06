acfe8940ee62cc3477ec0b3715c1be9a076ac0f0
Bogdan Tudose <bogdan.a.tudose@gmail.com>
2020-12-06 00:54:54 -0500

    Update adv6.py

--- day6/adv6.py
+++ day6/adv6.py
@@ -4,12 +4,10 @@ Created on Sun Dec  6 00:08:29 2020
 
 @author: Bogdan Tudose
 """
-
 #%% Source files
 fPath = "day6/"
 # f = open(fPath+"d6DemoInputs.txt", "r")
 f = open(fPath+"d6ActualInputs.txt", "r")
-
 inputs = f.read()
 
 #%% Import package
@@ -21,14 +19,11 @@ counts = []
 for group in groups:
     answers = [x for x in group.replace("\n","")]
     answers = set(answers)
-    print(len(answers))
     counts.append(len(answers))
     
 print(np.sum(counts))
-
 #%% Part 2
 groups = [x for x in inputs.strip("\n").split("\n\n")]
-groupSizes = [len(x.split("\n")) for x in groups]
 
 counts = []
 for group in groups:
@@ -36,11 +31,9 @@ for group in groups:
     answers = [x for x in group.replace("\n","")]
     uniqueAnswers = set(answers)
     allAnswered = 0
-    for x in uniqueAnswers:
-        if answers.count(x) == groupSize:
+    for ans in uniqueAnswers:
+        if answers.count(ans) == groupSize:
             allAnswered += 1
-        
-    print(allAnswered)
     counts.append(allAnswered)
     
 print(np.sum(counts))
